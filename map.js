aae1baf0199f3030800ce2291e5f24892b677382
Chris <chris@Christophers-MacBook-Pro.local>
2013-07-25 15:48:57 -0500

    draw a styled google map, given origin, destination, and waypoint cities

--- map.js
+++ map.js
@@ -1,15 +1,59 @@
-var origin = 'chicago, il';
-var destination = 'san francisco, ca';
-var waypoints = [ { location: 'seattle, washington', stopover: false } ];
-var mapType = google.maps.MapTypeId.SATELLITE;
+google.maps.visualRefresh = true;
+
+var origin = 'hanoi';
+var destination = 'ho chi minh city';
+var waypoints = [
+  { location: 'da lang, vietnam', stopover: true },
+  { location: 'nha trang, vietnam', stopover: true },
+]; 
+
+// = [ 
+//   { location: 'venice', stopover: true },
+//   { location: 'prague', stopover: true }, 
+//   { location: 'berlin', stopover: true }, 
+//   { location: 'amsterdam', stopover: true },
+//   { location: 'paris', stopover: true },
+//   { location: 'london', stopover: true }   
+// ];
+var mapType = google.maps.MapTypeId.ROADMAP;
 var travelMode = google.maps.TravelMode.DRIVING;
 
-function initialize() {        
+function initialize() { 
+  var styles = [
+    {
+      stylers: [
+        { saturation: -70 },
+        { gamma: 1.5 }
+      ]
+    }
+  ];
+
   var elemId = "map-canvas";
-  var mapOptions = { mapTypeId: mapType };
+  var mapOptions = { 
+    mapTypeId: mapType,
+    styles: styles,
+    disableDoubleClickZoom: true,
+    draggable: false,
+    keyboardShortcuts: false,
+    mapTypeControl: false,
+    panControl: false,
+    scrollwheel: false,
+    streetViewControl: false,
+    zoomControl: false
+  };
   map = new google.maps.Map(document.getElementById(elemId), mapOptions);
+  renderRoute();
+}
+function renderRoute() {
   var renderer = new google.maps.DirectionsRenderer({
-    markerOptions: { visible: false }
+    draggable: true,
+    markerOptions: { 
+      visible: true
+    },
+    polylineOptions: {
+      strokeColor: "#000000",
+      strokeWeight: 3
+    }
   });
   renderer.setMap(map);
   var router = new google.maps.DirectionsService();
