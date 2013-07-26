6ad1205dd0900905ef8f232314a68980f19d93bf
Chris <chris@Christophers-MacBook-Pro.local>
2013-07-25 20:05:44 -0500

    inputs can be lat/long values; oo for places; pixel location doesn't work yet

--- map.js
+++ map.js
@@ -1,22 +1,51 @@
 google.maps.visualRefresh = true;
 
-var origin = 'hanoi';
-var destination = 'ho chi minh city';
-var waypoints = [
-  { location: 'da lang, vietnam', stopover: true },
-  { location: 'nha trang, vietnam', stopover: true },
-]; 
+var inputs = ['19.687166, 105.501166', '16.238333333, 107.989166667', '16.375666667, 107.719333333', '11.933666667, 108.16'];
 
-// = [ 
-//   { location: 'venice', stopover: true },
-//   { location: 'prague', stopover: true }, 
-//   { location: 'berlin', stopover: true }, 
-//   { location: 'amsterdam', stopover: true },
-//   { location: 'paris', stopover: true },
-//   { location: 'london', stopover: true }   
-// ];
+var geocoder = new google.maps.Geocoder();
+
+var Place = function(name) {
+  this.name = name;
+  this.getLatLng();
+}
+
+Place.prototype.getLatLng = function() {
+  var self = this;
+  geocoder.geocode({ address: this.name }, function(result) {
+    self.latLng = result[0].geometry.location;
+    // self.getPxLoc();
+  });
+}
+
+
+// CanvasOverlay.prototype.findPxLocs = function(){
+//   var projection = this.prototype.getProjection();
+//   Place.prototype.getPxLoc();
+// };
+
+
+// Place.prototype.getPxLoc = function() {
+//   this.pxLoc = projection.fromLatLngToContainerPixel(this.latLng);
+// }
+
+var places = [];
+for(var i=0; i < inputs.length; i++) {
+  places.push( new Place(inputs[i]) );
+}
+
+var origin = places[0].name;
+var destination = places[places.length - 1].name;
+var waypoints = [];
+for(var i=1; i < places.length - 1; i++) {
+  waypoints.push( { location: places[i].name, stopover: true } );
+}
+
 var mapType = google.maps.MapTypeId.ROADMAP;
 var travelMode = google.maps.TravelMode.DRIVING;
+
+var map;
+var projection;
+
 
 function initialize() { 
   var styles = [
@@ -27,7 +56,6 @@ function initialize() {
       ]
     }
   ];
-
   var elemId = "map-canvas";
   var mapOptions = { 
     mapTypeId: mapType,
@@ -42,6 +70,20 @@ function initialize() {
     zoomControl: false
   };
   map = new google.maps.Map(document.getElementById(elemId), mapOptions);
+  CanvasOverlay = function() {
+    this.setMap(map);
+  };
+  CanvasOverlay.prototype = new google.maps.OverlayView();
+  CanvasOverlay.prototype.onAdd = function() {};
+  CanvasOverlay.prototype.onRemove = function() {};
+  CanvasOverlay.prototype.draw = function() {};
+  overlay = new CanvasOverlay;
+
+
+
+  // debugger;
+  // CanvasOverlay.findPxLocs();
+
   renderRoute();
 }
 function renderRoute() {
@@ -65,6 +107,10 @@ function renderRoute() {
   }, function(result, status) {
     renderer.setDirections(result);
   });
+
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addListener(map, 'idle', function() {
+  projection = CanvasOverlay.getProjection();
+});
